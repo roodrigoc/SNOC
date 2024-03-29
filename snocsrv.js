eedcb2e02093aa6a78a153bf158688d4c572ed18
Rodrigo Costa <52783418+roodrigoc@users.noreply.github.com>
2024-03-29 13:29:56 -0300

    Update snocsrv.js

--- snocsrv.js
+++ snocsrv.js
@@ -214,10 +214,40 @@ async function updateDeviceStatus(ws, devices) {
     }
 }
 
-const caminhoShared-folder = path.join(__dirname, 'shared-folder.js');
+//enter network share path here
+const pastaCompartilhada = '\\\\192.168.100.50\\tmp'; 
 
-app.get('/shared-folder', (req, res) => {
-    res.sendFile(caminhoShared-folder);
+app.get('/ultimo-arquivo', (req, res) => {
+
+    const pastaAtual = pastaCompartilhada;
+
+    fs.readdir(pastaAtual, (err, files) => {
+        if (err) {
+            res.send(`The folder (${pastaAtual}) don't exist.`);
+        } else {
+            if (files.length === 0) {
+                res.send(`The folder (${pastaAtual}) is empty.`);
+            } else {
+                files = files.filter(file => {
+			const lowercaseFile = file.toLowerCase();
+			return lowercaseFile.endsWith('.wav') || lowercaseFile.endsWith('.mp3');
+		});
+                if (files.length === 0) {
+                    res.send(`The folder (${pastaAtual}) don't contain audio files.`);
+                } else {
+                    files.sort((a, b) => {
+                        const statA = fs.statSync(path.join(pastaAtual, a));
+                        const statB = fs.statSync(path.join(pastaAtual, b));
+                        return statB.birthtime.getTime() - statA.birthtime.getTime();
+                    });
+
+                    const ultimoArquivo = files[0]; 
+                    const caminhoCompleto = path.join(pastaAtual, ultimoArquivo);
+                    res.send(caminhoCompleto);
+                }
+            }
+        }
+    });
 });
 
 const caminhoDevicesJS = path.join(__dirname, 'devices.js');
